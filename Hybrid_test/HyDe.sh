71b1ff950cb434ee923caeedda3c44402d933413
willright28 <48339510+willright28@users.noreply.github.com>
2025-07-27 15:30:49 +0800

    Create HyDe.sh

---
+++ Hybrid_test/HyDe.sh
@@ -0,0 +1,9 @@
+
+#/disk3/hkqm/hybrid/HyDe_chen/HyDe/scripts/individual_hyde_mp.py \
+#	-i hkqm.all.hyde.phy -m ind.list -o Western -tr triples.txt -n 72 \
+#	-t 22 -s 265739677 --prefix hkqm.all
+
+#/disk3/hkqm/hybrid/HyDe_chen/HyDe/scripts/individual_hyde_mp.py \
+#	-i hkqm.all.hyde.phy -m ind.list -o Out -tr triples3.txt -n 72 \
+#	-t 22 -s 265739677 --prefix hkqm.all.2th
+
