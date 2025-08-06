69e92549503969cb5199f8bd9e1a1c65d3a41e02
willright28 <48339510+willright28@users.noreply.github.com>
2025-08-06 17:14:11 +0800

    Update triangulaR.R

--- Hybrid_test/triangulaR.R
+++ Hybrid_test/triangulaR.R
@@ -1,51 +1,38 @@
-library(triangulaR)
-# library(vcfR)
-# devtools::install_github("omys-omics/triangulaR")
-# Read in data
-######
-sample=read.csv('/mnt/disk1/zhangshuai/hkqm/genetic_offset/pop.0.9.csv',header=T)
-sample=sample[sample$sample %in% ind.71,]
-head(sample)
-sample_mix=sample[,c("sample","mix")]
-colnames(sample_mix)=c("id","pop")
-sample_mix
-#####
-sample_mix_ec=filter(sample_mix,pop!="Western")
-write.table(sample_mix_ec$id,"/mnt/disk1/zhangshuai/hkqm/af_shifts/e_c.id",col.names=F,row.names = F,quote=F)
-sample_mix_wc=filter(sample_mix,pop!="Eastern")
-write.table(sample_mix_wc$id,"/mnt/disk1/zhangshuai/hkqm/af_shifts/w_c.id",col.names=F,row.names = F,quote=F)
-
-
-system("
-plink2 --vcf /mnt/disk1/zhangshuai/hkqm/gghybrid/vcf71.8.60.maf.0.05.ld.nomissing.vcf   --aec --keep /mnt/disk1/zhangshuai/hkqm/af_shifts/e_c.id --export vcf --out /mnt/disk1/zhangshuai/hkqm/gghybrid/ec
- ")
-
-system("
-plink2 --vcf /mnt/disk1/zhangshuai/hkqm/gghybrid/vcf71.8.60.maf.0.05.ld.nomissing.vcf   --aec --keep /mnt/disk1/zhangshuai/hkqm/af_shifts/w_c.id --export vcf --out /mnt/disk1/zhangshuai/hkqm/gghybrid/wc")
-
 options(vsc.dev.args = list(width = 600, height = 500))
+library(triangulaR)
+library(vcfR)
+library(dplyr)
+######
+sample=read.csv('./hybrid_test_pop_info.csv',header=T)
+sample_mix=sample[,c("sample","label")]
+colnames(sample_mix)=c("id","pop")
+
+#############test 1##########################
+sample_mix_ec=filter(sample_mix,pop!="fratercula")
+write.table(sample_mix_ec$id,"./e_c.id",col.names=F,row.names = F,quote=F)
+system("
+plink2 --vcf /mnt/disk1/zhangshuai/hkqm/gghybrid/vcf71.8.60.maf.0.05.ld.nomissing.vcf   --aec --keep ./e_c.id --export vcf --out ./hueti_davidi")
+
+data_hueti_davidi <- read.vcfR("/mnt/disk1/zhangshuai/hkqm/gghybrid/hueti_davidi.vcf", verbose = F)
+example.vcfR.diff.1 <- alleleFreqDiff(vcfR =data_hueti_davidi , pm = sample_mix_ec, p1 = "davidi", p2 = "hueti", difference = 0.95)
+hi.het.1 <- hybridIndex(vcfR = example.vcfR.diff.1 , pm = sample_mix_ec, p1 = "davidi", p2 = "hueti")
+
+#pdf("/mnt/disk1/zhangshuai/hkqm/gghybrid/output.pdf", width = 7, height = 5) 
+triangle.plot(hi.het.1,cex=3, max.overlaps = 1)
+#dev.off() 
 
 
-data_ec <- read.vcfR("/mnt/disk1/zhangshuai/hkqm/gghybrid/ec.vcf", verbose = F)
-data_ec
-# Create a new vcfR object composed only of sites above the given allele frequency difference threshold
-example.vcfR.diff_ec <- alleleFreqDiff(vcfR = data_ec , pm = sample_mix_ec, p1 = "Central", p2 = "Eastern", difference = 0.95)
+#############test 2##########################
+sample_mix_wc=filter(sample_mix,pop!="hueti")
+write.table(sample_mix_wc$id,"./w_c.id",col.names=F,row.names = F,quote=F)
 
-hi.het.ec <- hybridIndex(vcfR = example.vcfR.diff_ec, pm = sample_mix_ec, p1 = "Central", p2 = "Eastern")
-hi.het.ec
-pdf("/mnt/disk1/zhangshuai/hkqm/gghybrid/output.pdf", width = 7, height = 5) 
-triangle.plot(hi.het.ec,cex=3, max.overlaps = 1)
-dev.off() 
+system("
+plink2 --vcf /mnt/disk1/zhangshuai/hkqm/gghybrid/vcf71.8.60.maf.0.05.ld.nomissing.vcf   --aec --keep ./w_c.id --export vcf --out ./fratercula_davidi")
 
-
-data_wc <- read.vcfR("/mnt/disk1/zhangshuai/hkqm/gghybrid/wc.vcf", verbose = F)
-data_wc
-sample_mix_wc
-# Create a new vcfR object composed only of sites above the given allele frequency difference threshold
-example.vcfR.diff_wc <- alleleFreqDiff(vcfR = data_wc, pm = sample_mix_wc, p1 = "Central", p2 = "Western", difference = 0.95)
-
-hi.het.wc <- hybridIndex(vcfR = example.vcfR.diff_wc, pm = sample_mix_wc,  p1 = "Central", p2 = "Western")
-hi.het.wc
-pdf("/mnt/disk1/zhangshuai/hkqm/gghybrid/output2.pdf", width = 7, height = 5) 
-triangle.plot(hi.het.wc,cex=3, max.overlaps = 3)
-dev.off() 
+data_fratercula_davidi <- read.vcfR("/mnt/disk1/zhangshuai/hkqm/gghybrid/fratercula_davidi.vcf", verbose = F)
+example.vcfR.diff.2 <- alleleFreqDiff(vcfR = data_fratercula_davidi, pm = sample_mix_wc, p1 = "davidi", p2 = "fratercula", difference = 0.95)
+hi.het.2 <- hybridIndex(vcfR = example.vcfR.diff.2, pm = sample_mix_wc,  p1 = "davidi", p2 = "fratercula")
+hi.het.2
+#pdf("/mnt/disk1/zhangshuai/hkqm/gghybrid/output2.pdf", width = 7, height = 5) 
+triangle.plot(hi.het.2,cex=3, max.overlaps = 1)
+#dev.off() 
