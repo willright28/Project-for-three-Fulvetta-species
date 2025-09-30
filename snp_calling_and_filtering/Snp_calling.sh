77a0a4dcceec9e0b5b01df2c922022ce33aa1db4
willright28 <48339510+willright28@users.noreply.github.com>
2025-09-30 09:54:08 +0800

    Update SNP_calling.sh

--- snp_calling_and_filtering/Snp_calling.sh
+++ snp_calling_and_filtering/Snp_calling.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 ###################
-###################
+workdir=/disk3/call_snp/vcf96
+ref=/disk3/ref/CL100120897_L02.gapcloser.FINAL.fasta
 
-set -e
-set -x 
-set -o pipefail
+#SNP calling was run in parallel on split chromosome files, and the per-chromosome VCFs were concatenated to  the final whole-genome VCF
+awk '{print $1, 1 ,$2}' /disk3/ref/CL100120897_L02.gapcloser.FINAL.fasta.fai > ${workdir}/split.txt
 
-workdir=/disk3/hkqm/call_snp/vcf96
-ref=/disk3/zhangshuai3/hkqm/ref_hkqm/CL100120897_L02.gapcloser.FINAL.fasta
+awk '{print $1}' ${workdir}/id.list.txt | sed "s:^:/disk3/realig_bam/:" |sed "s:$:.mem_map.sort.mkd.realign.bam:" > ${workdir}/bam.out.list.txt
+while read line ; do { if [ -f $line ];then echo "yes" ; else echo "no" ; fi }; done < ${workdir}/bam.out.list.txt
 
-#awk '{print $1, 1 ,$2}' /disk3/zhangshuai3/hkqm/ref_hkqm/CL100120897_L02.gapcloser.FINAL.fasta.fai > /disk3/hkqm/call_snp/vcf96/split.txt
-
-#awk '{print $1}' /disk3/hkqm/call_snp/vcf96/callsnp.list.txt | sed "s:^:/disk3/zhangshuai3/hkqm/realig_bam/:" |sed "s:$:.mem_map.sort.mkd.realign.bam:" > /disk3/hkqm/call_snp/vcf96/bam.out.list.txt
-
-#while read line ; do { if [ -f $line ];then echo "yes" ; else echo "no" ; fi }; done < /disk3/hkqm/call_snp/vcf96/bam.out.list.txt
 
 thread=60
 temfifo=$$.fifo
@@ -32,21 +27,17 @@ if [ $chr ]
 then
 read -u9
 {
-
-#分开callsnp
-#mpileup:producing genotype likelihoods;#http://samtools.github.io/bcftools/bcftools.html
-
-time  bcftools mpileup -f $ref -r ${chr}:${sta}-${end} -b ${workdir}/bam.out.list.txt \
---annotate FORMAT/AD,FORMAT/ADF,FORMAT/ADR,FORMAT/DP,FORMAT/SP,INFO/AD,INFO/ADF,INFO/ADR | bcftools call -m -Oz -f GQ -o ${workdir}/raw_vcf_out/${chr}.bcftools.chr.vcf.gz
+#call snp
+    bcftools mpileup -f $ref -r ${chr}:${sta}-${end} -b ${workdir}/bam.out.list.txt \
+    --annotate FORMAT/AD,FORMAT/ADF,FORMAT/ADR,FORMAT/DP,FORMAT/SP,INFO/AD,INFO/ADF,INFO/ADR | bcftools call -m -Oz -f GQ -o ${workdir}/raw_vcf_out/${chr}.bcftools.chr.vcf.gz
     echo >&9
 }&
 fi
-done< /disk3/hkqm/call_snp/vcf96/split.more.txt
+done< ${workdir}/split.txt
 wait
 
-awk '{print $4}' /disk3/hkqm/call_snp/vcf96/split.more.txt | sed "s:^:/disk3/hkqm/call_snp/vcf96/raw_vcf_out/:" | sed "s:$:.bcftools.chr.vcf.gz:" > /disk3/hkqm/call_snp/vcf96/chr.raw.list.txt 
-
-time bcftools concat --threads 70 -Oz -o /disk3/hkqm/call_snp/vcf96/htcw.bcftools.raw.out.vcf.gz -f  /disk3/hkqm/call_snp/vcf96/chr.raw.list.txt
+awk '{print $4}' ${workdir}/split.txt | sed "s:^:${workdir}/raw_vcf_out/:" | sed "s:$:.bcftools.chr.vcf.gz:" > ${workdir}/chr.raw.list.txt 
+time bcftools concat --threads 70 -Oz -o  ${workdir}/all.bcftools.raw.out.vcf.gz -f   ${workdir}/chr.raw.list.txt
 
 exec 9>$-
 
