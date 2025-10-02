77d6f42c5e2bd05443bbbe2c3f78d93bfb1c581c
willright28 <48339510+willright28@users.noreply.github.com>
2025-10-02 13:37:14 +0800

    Update NJ_tree.sh

--- Genetic_structure_analysis/NJ_tree.sh
+++ Genetic_structure_analysis/NJ_tree.sh
@@ -5,10 +5,11 @@ set -o pipefail
 
 ### Generate a pairwise genetic distance matrix using VCF2Dis ###
 /home/share/VCF2Dis-1.42/bin/VCF2Dis \
-  -InPut ./maf.0.05.ld.vcf \
+  -InPut /disk3/call_snp/plink/vcf71.${min_dp}.${max_dp}.maf.0.05.ld.vcf \
   -OutPut ./vcf2dis.dist
 
 ### Construct the neighbor-joining (NJ) tree using the FastME web server ###
 # Upload the output file `vcf2dis.dist` to the FastME online platform:
 # http://www.atgc-montpellier.fr/fastme/
 # Choose the BioNJ algorithm
+
