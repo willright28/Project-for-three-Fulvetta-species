7ab4491904b029e8666de8cba83f4441a6906181
willright28 <48339510+willright28@users.noreply.github.com>
2025-10-02 16:31:43 +0800

    Update PSMC.sh

--- PSMC.sh
+++ PSMC.sh
@@ -1,1 +1,7 @@
-psmc -N25 -t15 -r5 -b -p "4+30*2+4+6+10" -o FJ369_{}.pasmc /disk3/hkqm/psmc/psmc_data/FJ369/FJ369.split.psmc.fa
+#!/bin/bash
+
+bcftools mpileup -C 50 -q 20 -Ou -f ${ref} ./realig_bam/${id}.mem_map.sort.mkd.realign.bam | bcftools call -c - |\
+/home/share/bcftools-1.9/misc/vcfutils.pl vcf2fq -d 4 -D 50 > ./psmc_data/${id}.psmc.fq
+
+fq2psmcfa -q20 ./psmc_data/${id}.psmc.fq > ./psmc_data/${id}.psmcfa
+psmc -N30 -t5 -r5 -p "4+30*2+4+6+10" -o ./psmc_data/${id}.psmc ./psmc_data/${id}.psmcfa
