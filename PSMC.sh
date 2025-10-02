4d7a292d4a55a182f3a9fb17bd4c3ddefd47cc12
willright28 <48339510+willright28@users.noreply.github.com>
2025-10-02 16:34:25 +0800

    Update PSMC.sh

--- PSMC.sh
+++ PSMC.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+ref=$1
+id=$2
 bcftools mpileup -C 50 -q 20 -Ou -f ${ref} ./realig_bam/${id}.mem_map.sort.mkd.realign.bam | bcftools call -c - |\
 /home/share/bcftools-1.9/misc/vcfutils.pl vcf2fq -d 4 -D 50 > ./psmc_data/${id}.psmc.fq
 
