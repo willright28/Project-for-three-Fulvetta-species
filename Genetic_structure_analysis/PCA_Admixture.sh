239487d913d6aa54085abd369671c42773886af3
willright28 <48339510+willright28@users.noreply.github.com>
2025-10-02 13:43:52 +0800

    Update PCA_Admixture.sh

--- Genetic_structure_analysis/PCA_Admixture.sh
+++ Genetic_structure_analysis/PCA_Admixture.sh
@@ -4,14 +4,15 @@ set -e
 set -o pipefail
 
 ###### PCA analysis #####
-plink2 --vcf ./maf.0.05.ld.vcf --aec --pca --out ./pca
+plink2 --vcf /disk3/call_snp/plink/vcf71.${min_dp}.${max_dp}.maf.0.05.ld.vcf --aec --pca --out ./pca
 
 ###### Admixture analysis for K=1 to 5 #####
+plink2 --vcf /disk3/call_snp/plink/vcf71.${min_dp}.${max_dp}.maf.0.05.ld.vcf  --aec 0 --make-bed -out /disk3/call_snp/plink/vcf71.${min_dp}.${max_dp}.maf.0.05.ld.str
+#
 for k in {1..5}
 do
-  /home/share/admixture_linux-1.3.0/admixture --cv ./str.bed $k -j10 | tee ./admix.log${k}.out &
+  /home/share/admixture_linux-1.3.0/admixture --cv /disk3/call_snp/plink/vcf71.${min_dp}.${max_dp}.maf.0.05.ld.str.bed $k -j10 | tee ./admix.log${k}.out &
 done
-
 wait
 
 ###### Extract cross-validation (CV) errors #####
