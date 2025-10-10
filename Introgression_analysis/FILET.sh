9764a09d01530c7698f1c66259e01edcdd467b1b
willright28 <48339510+willright28@users.noreply.github.com>
2025-10-10 17:15:22 +0800

    Update FILET.sh

--- Introgression_analysis/FILET.sh
+++ Introgression_analysis/FILET.sh
@@ -7,7 +7,7 @@ cat ./FILET/simdata/mig21.w2c.msOut | ./FILET/FILET-master/twoPopnStats_forML 18
 
 #2. Aggregate our training data into one labeled data matrix to be used for training.
 python2  ./FILET/FILET-master/buildThreeClassTrainingSet.py ./FILET/test/trainingSimsStats/ ./FILET/test/trainingSets/threeClass.fvec
-#3. Train the classifier.
+#3. Train the classifier. Run multiple times to obtain stable results.
 python2 ./FILET/FILET-master/trainFiletClassifier.py ./FILET/test/trainingSets/threeClass.fvec ./FILET/test/classifier/threeClass.p pi1 pi2 tajd1 tajd2 hetVar1 hetVar2 ss1 ss2 ddRank1 ddRank2 zx Fst gmin dxy_mean dxy_min snn 
 
 #4. Input  real phased sequence data set
@@ -17,5 +17,6 @@ do
 ./FILET/FILET-master/pgStatsBedSubpop_forML  ./phase/fa.split/eastern_fa/eastern.${line}.fa  ./phase/fa.split/central_fa/central.${line}.fa  ./phase/fa.split/hkqm.${line}.fasta  ./phase/fa.split/${line}.win.bed 0.5 | python2 ./FILET/FILET-master/normalizePgStats.py 10000 > ./FILET/test/featureVectorsToClassify/e2c.${line}.ss
 done <  /disk3/hkqm/phase/chr_.txt
 
-#5. Use classifier to produce classifications on the real data set
+#5. Use classifier to produce classifications on the real data set. To obtain stable results, combine the outputs of multiple classifiers in step#3.
 python2 /disk3/hkqm/FILET/FILET-master/classifyChromosome.py ./FILET/test/classifier/threeClass.p ./FILET/test/featureVectorsToClassify/  0.01 /disk3/hkqm/FILET/test/results
+
